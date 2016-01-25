49dd2658f4a5da801298d9e7ce65204a823accfc
lukereding <lukereding@gmail.com>
2016-01-25 16:31:46 -0600

    update script

--- show_vid.py
+++ show_vid.py
@@ -5,16 +5,14 @@ import pyglet, sys
 '''
 python script to run two different videos on two separate screens attached to the computer.
 run like `python show_vid.py path/video1 path/video2`
+the script will exit if you have fewer or more than two screens connected
 '''
-
-## to do:
-## - set the window size equal to the size of each screen
 
 # get information about the screens. print to the screen
 all_screens = pyglet.window.get_platform().get_default_display().get_screens()
 print all_screens
 
-# get sizes of all screens
+# print the sizes of all screens to the user
 for screen in all_screens:
     print "size of screens:\n"
     print screen.width, screen.height
@@ -37,21 +35,27 @@ print "the second video is %s seconds long" %{mov2.duration}
 print 'first video size=[%i,%i]' %(mov1.format.width, mov1.format.height)
 print 'second video size=[%i,%i]' %(mov2.format.width, mov2.format.height)
 
+# start the clock for timing
 globalClock = core.Clock()
 
 while globalClock.getTime()<(mov1.duration+60):
+    # draw the videos
     mov1.draw()
     mov2.draw()
-    if globalClock.getTime() > mov1.duration:
-        text = visual.TextStim(mainWin, text="trial ended (!)",pos=(0,-0.6),alignVert='bottom', color='SlateGrey')
+    
+    # if the trial is ended, let the user know:
+    if globalClock.getTime() > mov1.duration+10:
+        text = visual.TextStim(mainWin, text="trial ended (!)", pos=(0,-0.6), alignVert='bottom', color='SlateGrey')
     else:
-        text = visual.TextStim(mainWin, text=str(globalClock.getTime()),pos=(0,-0.6),alignVert='bottom', color='SlateGrey')
+        text = visual.TextStim(mainWin, text=str(round(globalClock.getTime(),0)), pos=(0,-0.6), alignVert='bottom', color='SlateGrey')
+    
+    # draw the time, update the windows
     text.draw()
     mainWin.update()
     secondWin.update()
     
     # if a key has been pressed, exit out of the program
-    if len(event.getKeys())>0: break
-    event.clearEvents()
+    # if len(event.getKeys())>0: break
+    # event.clearEvents()
 
 core.quit()
