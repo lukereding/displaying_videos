36c875f9291dfee62c8749f121fada4cb8620f6f
lukereding <lukereding@gmail.com>
2016-01-26 10:26:22 -0600

    add screen class to separate .py file

---
+++ screen.py
@@ -0,0 +1,37 @@
+import pyglet
+from psychopy import visual, core, event
+
+class Screen:
+    
+    '''
+    screen class! 
+    use it to define your screen and what video it will play
+    example: Screen("screen1", all_screens[0], /Users/lukereding/Documents/blender_files/transitivity/size/small_vs_large2.mp4, 0)
+    '''
+    
+    def __init__(self, name, monitor, video_path, number):
+        self.name = name
+        self.width = monitor.width
+        self.height = monitor.height
+        self.window = visual.Window([monitor.width, monitor.height], units='norm', fullscr=False, screen=number)
+        self.video =  visual.MovieStim(self.window, video_path, flipVert=False)
+        self.video_width = int(self.video.format.width)
+        self.video_height = int(self.video.format.height)
+        self.video_path = video_path
+        self.duration = self.video.duration
+        
+    def print_monitor_size(self):
+        print "{} has height of {} and width of {}.".format(self.name, self.width, self.height)
+    
+    def print_video_size(self):
+        print " the video {} is {} x {}".format(self.video_path, self.video_height, self.video_width)
+    
+    def print_duration(self):
+        print "{} is {} s long".format(self.video_path, self.duration)
+    
+    def draw(self):
+        self.video.draw()
+    
+    def update(self):
+        self.window.update()
+screens
