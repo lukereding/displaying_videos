aae50f3567bec76386c7db76a8322683ef5f5d8f
lukereding <lukereding@gmail.com>
2016-04-04 09:34:17 -0500

    change acclimation trials to 2 hours from 1 hour

--- run_acclimation_trials.sh
+++ run_acclimation_trials.sh
@@ -13,11 +13,11 @@ VOICE=(
 #pick a random voice
 rand=$[ $RANDOM % ${#VOICE[@]} ]
 RANDOM_VOICE=${VOICE[$rand]}
-LENGTH_OF_VIDEOS=3600
+LENGTH_OF_VIDEOS=7200
 
 mini1=lukereding@128.83.192.234
 
-echo -e "this script runs the background video, which should be 60 minutes long, on all three screens\n\n"
+echo -e "this script runs the background video, which should be 2 hours long, on all three screens\n\nthe script will email you when it is done"
 
 # do you want to record the acclimation?
 while true; do
@@ -30,8 +30,7 @@ while true; do
 done
 
 # start the trial
-
-say -v $RANDOM_VOICE "Pressing Y and Enter on the keyboard will start the acclimation for 1 hour."
+say -v $RANDOM_VOICE "Pressing Y and Enter on the keyboard will start the acclimation for 2 hours."
 
 while true; do
     read -p "type Y then press Enter when you're ready to begin the acclimation:." y
@@ -51,11 +50,14 @@ if [ "$record" == "yes" ]; then
     echo "sleep 14; ffmpeg -f avfoundation -video_size 1280x720 -framerate 10 -i "Micro:none" -crf 28 -vcodec libx264 -y -t "$LENGTH_OF_VIDEOS" ~/Desktop/acclimation_trials/"`date "+%Y.%m.%d.%H.%M.%S"`"".avi" || echo "video failed"" | ssh $mini1 /bin/bash &
 fi
 
-# show the background60min/mp4 video on all three screens
+# show the background60min.mp4 video on all three screens
 echo "cd `pwd` && python show_vid.py -v1 background60min.mp4 -t "$START_TIME"" | ssh $mini1 /bin/bash &
 python show_vid.py -v1 background60min.mp4 -v2 background60min.mp4 -t "$START_TIME" &
 
 wait
+
+# email
+echo "the acclimation trial is over at `date`" | mail -s "acclimation trial over!" lukereding@gmail.com
 
 say -v $RANDOM_VOICE "The acclimation trial is over. Exiting the program now. Smell you later."
 
